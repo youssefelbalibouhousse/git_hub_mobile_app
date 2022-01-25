e58a61fda5bb0b1d34b6a04dc66552ab34cd66db
youssefelbalibouhousse <youssef.elbali@gmail.com>
2022-01-25 19:16:48 +0100

    Ajout du formulaire dans un container afin de le manipuler Ajout de l'icone de recherche d'

--- lib/pages/users/user.page.dart
+++ lib/pages/users/user.page.dart
@@ -6,7 +6,38 @@ class UsersPage extends StatelessWidget{
     return Scaffold(
       appBar: AppBar(title: Text('User'),),
       body: Center( // permet de centrer le texte child
-        child: Text('User', style:Theme.of(context).textTheme.headline2,),
+        child: Column(
+          children: [
+            Row(
+              children: [// debut de ligne
+                Expanded( //pour occuper la largeur de toute la page
+                  child: Container( //Ici l'emploi du container sur TextFormFields permet de manipuler un bloc et le centrer via padding
+                      padding: EdgeInsets.all(10),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                            suffixIcon: Icon(Icons.visibility),//icone de masquage a droite du formulaire
+                            //icon: Icon(Icons.local_cafe), icone a gauche
+                            contentPadding: EdgeInsets.all(10), //espace entre texte et bordure
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10), //arrondie du formulaire
+                                borderSide: BorderSide(
+                                    width: 1 , color: Colors.indigo
+                                )
+                            )
+                        ),
+                      )
+                  ),
+                ),
+                IconButton(
+                    onPressed: null,
+                    icon: Icon(Icons.search , color: Colors.indigo,)
+                )
+
+              ],
+            )
+
+          ],
+        )
       ),
     );
   }
